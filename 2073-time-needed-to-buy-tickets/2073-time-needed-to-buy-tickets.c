1c8d85de17ecf1cc541e4faa8daecb0f3602ee2d
Vyswi Katta <vyswikatta21@gmail.com>
2026-09-13 19:09:54 +0530

    Time: 0 ms (100%), Space: 8.9 MB (40.43%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.c
@@ -0,0 +1,13 @@
+int timeRequiredToBuy(int* tickets, int ticketsSize, int k) {
+    int time = 0;
+
+    for (int i = 0; i < ticketsSize; i++) {
+        if (i <= k) {
+            time += (tickets[i] < tickets[k]) ? tickets[i] : tickets[k];
+        } else {
+            time += (tickets[i] < tickets[k]) ? tickets[i] : tickets[k] - 1;
+        }
+    }
+
+    return time;
+}
