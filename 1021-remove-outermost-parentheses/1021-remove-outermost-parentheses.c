16a9fa9f8bb5ba936721cb020ae277414b84f09b
Vyswi Katta <vyswikatta21@gmail.com>
2026-09-13 19:16:14 +0530

    Time: 0 ms (100%), Space: 8.9 MB (62.53%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.c
@@ -0,0 +1,30 @@
+char* removeOuterParentheses(char* s) {
+    int n = strlen(s);
+    char* result = malloc((n + 1) * sizeof(char));
+
+    int balance = 0;
+    int j = 0;
+
+    for (int i = 0; i < n; i++) {
+
+        if (s[i] == '(') {
+            // Add '(' only if it is not the outermost one
+            if (balance > 0) {
+                result[j++] = s[i];
+            }
+            balance++;
+        }
+        else {
+            balance--;
+
+            // Add ')' only if it is not the outermost one
+            if (balance > 0) {
+                result[j++] = s[i];
+            }
+        }
+    }
+
+    result[j] = '\0';
+
+    return result;
+}
